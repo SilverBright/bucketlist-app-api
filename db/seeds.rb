8c2e6017db5c282f24c5d8c6cd65f6e278fbc0b4
github username <github email address>
2019-06-02 15:40:32 -0700

    drop database, re-migrate, seed new data

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,8 +8,8 @@
 
 
 
-Idea.create(id: 1, body: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. ')
-Idea.create(id: 2, body: 'Prow scuttle parrel provost Sail ho shrouds spirits boom mizzenmast yardarm.')
-Idea.create(id: 3, body: 'Caramels muffin jelly beans sweet cheesecake gummi bears danish dessert fruitcake.')
-Idea.create(id: 4, body: 'Typewriter stumptown helvetica williamsburg scenester, ugh activated charcoal deep v narwhal.') 
-Idea.create(id: 5, body: 'Vitae sequi. Sequi quia. Rem culpa yet culpa yet magna or quia, so nesciunt.') 
+Idea.create(id: 1, body: 'Be a Contestant on a Game Show.')
+Idea.create(id: 2, body: 'Participate in a Disneyland parade.')
+Idea.create(id: 3, body: 'Change my Name.')
+Idea.create(id: 4, body: 'Color my hair purple.') 
+Idea.create(id: 5, body: 'Write a book.') 
